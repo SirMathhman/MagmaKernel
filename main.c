4de4746f5e63553d5998adbf22b7379938877deb
SirMathhman <sirmathhman@gmail.com>
2020-01-11 17:21:16 -0500

    Add Instance methods

--- main.c
+++ main.c
@@ -1,18 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef void *Any;
+typedef Any *Instance;
+
+Instance new(int size) {
+    return malloc(size * sizeof(Any));
+}
+
+void delete(Instance instance) {
+    free(instance);
+}
+
+Any get(Instance instance, int index) {
+    return instance[index];
+}
+
+void set(Instance instance, int index, Any value) {
+    instance[index] = value;
+}
+
+typedef Instance StringBuilder;
+
+void **StringBuilder() {
+    malloc(2 * sizeof(void *));
+}
+
+void test() {
+
+}
+
 int main() {
-    FILE* source = fopen("main.magma", "r");
+    FILE *source = fopen("main.magma", "r");
     fseek(source, 0, SEEK_END);
     int length = ftell(source);
     fseek(source, 0, SEEK_SET);
-    char* buffer = malloc(length);
-    if(buffer) {
+    char *buffer = malloc(length);
+    if (buffer) {
         fread(buffer, 1, length, source);
     }
     fclose(source);
-    char* toWrite = "";
-    FILE * out = fopen("out.c", "w");
+    char *toWrite = "";
+    FILE *out = fopen("out.c", "w");
     fputs("#include <stdio.h>\nint main(){printf(\"Hello World!\");}", out);
     fclose(out);
 
