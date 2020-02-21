7a469029fbb1a0d625b573c5b41eb771afe86cdd
SirMathhman <sirmathhman@gmail.com>
2020-02-21 00:30:54 -0500

    Add Throwable.c

---
+++ Throwable.c
@@ -0,0 +1,19 @@
+//
+// Created by SirMathhman on 2/21/2020.
+//
+
+#include "Throwable.h"
+
+char *getMessage_Throwable(Object *this, Throwable (*cast)(Object *)) {
+    return cast(this).message;
+}
+
+Throwable Throwable_(char *message) {
+    Object Object = {Throwable_ID, NULL, NULL, NULL};
+    Throwable result = {Object, message};
+    return result;
+}
+
+void Throwable$(Object *this, Throwable (*cast)(Object *)) {
+    free(getMessage_Throwable(this, cast));
+}
---
+++ Throwable.h
@@ -0,0 +1,24 @@
+//
+// Created by SirMathhman on 2/21/2020.
+//
+
+#ifndef MAGMAKERNEL_THROWABLE_H
+#define MAGMAKERNEL_THROWABLE_H
+
+#include <stdlib.h>
+#include <stddef.h>
+#include "core.h"
+
+static const long Throwable_ID = 723654134L;
+typedef struct Throwable {
+    Object Object;
+    char *message;
+} Throwable;
+
+Throwable Throwable_(char *message);
+
+void Throwable$(Object *this, Throwable (*cast)(Object *));
+
+char *getMessage_Throwable(Object *this, Throwable (*cast)(Object *));
+
+#endif //MAGMAKERNEL_THROWABLE_H
