88d965286779164ae6cc0f4cc847d29f3ac680ea
SirMathhman <sirmathhman@gmail.com>
2020-02-20 22:51:53 -0500

    Add Vector

--- Collection.h
+++ Collection.h
@@ -5,4 +5,34 @@
 #ifndef MAGMAKERNEL_COLLECTION_H
 #define MAGMAKERNEL_COLLECTION_H
 
+#include "core.h"
+
+typedef struct Vector {
+    Object *Object;
+    Array *elements;
+    int size;
+} Vector;
+
+Vector Vector_empty();
+
+Vector Vector_of(int size);
+
+Any *get_Vector(Any *this, Vector *(*cast)(Any *), int index);
+
+Any *set_Vector(Any *this, Vector *(*cast)(Any *), int index, Any *element);
+
+void add_Vector(Any *this, Vector *(*cast)(Any *), Any *element);
+
+bool removeIndex_Vector(Any *this, Vector *(*cast)(Any *), int index);
+
+bool removeElement_Vector(Any *this, Vector *(*cast)(Any *), Any *element);
+
+bool contains(Any *this, Vector *(*cast)(Any *), Any *element);
+
+void clear(Any *this, Vector *(*cast)(Any *));
+
+bool isEmpty(Any *this, Vector *(*cast)(Any *));
+
+int size_Vector(Any *this, Object *(*cast)(Any *));
+
 #endif //MAGMAKERNEL_COLLECTION_H
